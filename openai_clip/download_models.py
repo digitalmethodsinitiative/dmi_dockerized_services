a7d5133090a2ac37c83c0e9babedadcaec19a899
Dale Wahl <dalewahl@gmail.com>
2023-06-28 12:55:18 +0200

    fix model downloader

--- openai_clip/download_models.py
+++ openai_clip/download_models.py
@@ -1,5 +1,7 @@
 import clip
+import torch
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 for model_name in clip.available_models():
+    print(f"Downloading {model_name} model...\n")
     model, preprocess = clip.load(model_name, device=device)
