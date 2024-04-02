cacc7cb87b27859a5b4b9882612219594740a7b6
Stijn Peeters <stijn.peeters@uva.nl>
2024-04-02 12:16:05 +0200

    Image classifier Docker image

---
+++ image_classifier/README.md
@@ -0,0 +1,57 @@
+# Image classification Docker image
+
+This Docker image runs a simple script to run a number of image classification models from HuggingFace on a given image
+dataset. It reports all labels an image is annotated with by any of the models with at least a certain confidence level.
+
+### Build the Docker image
+1. Navigate to this folder
+2. Install the models:
+   - `git clone --depth 1 https://huggingface.co/google/vit-base-patch16-224`
+   - `git clone --depth 1 https://huggingface.co/tonyassi/celebrity-classifier`
+   - `git clone --depth 1 https://huggingface.co/Falconsai/nsfw_image_detection`
+   These are pre-loaded this way because `git clone` is not cached by Docker builds, so any subsequent builds would need
+   to download the full models and weights again, which can take a long time. Once you are happy with the image 
+   (probably immediately after building) you can delete these repositories from the folder. You may need to install 
+   `git-lfs` (for example via `apt install` first).
+3. Build the image: `docker build -t image_classifier .` 
+
+The build process pre-loads the models from Hugging Face. If you want to add other models or change the ones used, 
+simply edit the references in `classifier.py` and adjust the build accordingly.
+
+### Run the container persistently 
+Run container as daemon from image
+
+`docker run -v $(pwd)/data/:/app/data/ --name image_classifier --gpus all -d image_classifier`
+-  `-v $(pwd)/data/:/app/data/` mounts the `data` directory in your current working directory to the container
+- `--name image_classifier` names the container "stable_diffusion"
+- `--gpus all` is needed for the container to use the host GPUs; remove and it will run without GPUs albeit MUCH more 
+  slowly (unusably, basically)
+- `-d` runs the container and disconnects
+
+### Run the container for a single use
+This Docker run command combines the above to create a one time use Docker container to run the prompt via the interface
+script discussed below.
+
+Assuming you have a folder `data` in your current working directory with files in it, run:
+`docker run --rm -v $(pwd)/data/:/app/data/ --gpus all image_classifier bash -c "python3 classifier.py --help"`
+
+### Output
+Annotations are saved as an .ndjson file, with one line per classified image, formatted as follows:
+
+```json
+{
+  "image-filename.jpg": {
+    "features": {
+      "web site, website, internet site, site": 0.9856060147285461,
+      "comic book": 0.6124848127365112
+    },
+    "celebrities": {
+      "Johnny Depp": 0.99977010238953,
+      "Keira Knightley": 0.7143849120365015
+    },
+    "nsfw": {
+      "normal": 0.9997770190238953
+    }
+  }
+}
+```
---
+++ image_classifier/classifier.py
@@ -0,0 +1,104 @@
+import argparse
+import numpy as np
+import json
+import torch
+import PIL
+
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+from pathlib import Path
+
+have_cuda = torch.cuda.is_available()
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+classifiers = {
+    "features": {
+        "model_name": "google/vit-base-patch16-224",
+        "model": None,
+        "preprocessor": None
+    },
+    "celebrities": {
+        "model_name": "tonyassi/celebrity-classifier",
+        "model": None,
+        "preprocessor": None
+    },
+    "nsfw": {
+        "model_name": "Falconsai/nsfw_image_detection",
+        "model": None,
+        "preprocessor": None
+    }
+}
+
+
+# Copied from transformers.pipelines.text_classification.softmax
+def softmax(_outputs):
+    maxes = np.max(_outputs, axis=-1, keepdims=True)
+    shifted_exp = np.exp(_outputs - maxes)
+    return shifted_exp / shifted_exp.sum(axis=-1, keepdims=True)
+
+
+def parse_args():
+    """
+    Parse command line arguments
+    """
+    cli = argparse.ArgumentParser()
+    cli.add_argument("--image-folder", "-i", help="Path to folder containing images", required=True)
+    cli.add_argument("--with-features", "-f", help=f"Annotate with ImageNet-based classes ({classifiers['features']['model_name']})", action="store_true", default=False)
+    cli.add_argument("--with-celebrities", "-c", help=f"Annotate with celebrity detection ({classifiers['celebrities']['model_name']})", action="store_true", default=False)
+    cli.add_argument("--with-nsfw", "-n", help=f"Annotate with NSFW detection ({classifiers['nsfw']['model_name']})", action="store_true", default=False)
+    cli.add_argument("--output-dir", "-o", help="Output directory where annotations will be saved", default="data",
+                     required=True)
+    cli.add_argument("--dataset-name", "-d", help="Dataset name (to use for output file)", required=True)
+    cli.add_argument("--label-threshold", "-t", help="Threshold for confidence in labels to be included in output (default 0.5, or 50%)", default=0.5, type=float)
+    return cli.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    output_folder = Path(args.output_dir)
+    if not output_folder.exists():
+        print(f"Output folder {args.output_dir} not found.")
+        exit(1)
+
+    for classifier, settings in classifiers.items():
+        if not getattr(args, f"with_{classifier}"):
+            continue
+
+        classifiers[classifier]["preprocessor"] = AutoImageProcessor.from_pretrained(settings["model_name"])
+        classifiers[classifier]["model"] = AutoModelForImageClassification.from_pretrained(settings["model_name"]).to(
+            device)
+
+    done = 0
+    with output_folder.joinpath(args.dataset_name + ".ndjson").open("w") as outfile:
+        images = Path(args.image_folder)
+        for image in images.glob("*"):
+            metadata = {}
+
+            try:
+                image_obj = PIL.Image.open(image)
+            except PIL.UnidentifiedImageError:
+                continue
+
+            if image_obj.size == (1, 1):
+                continue
+
+            if image_obj.mode != "RGB":
+                image_obj = image_obj.convert("RGB")
+
+            for classifier, settings in classifiers.items():
+                if not settings["preprocessor"]:
+                    continue
+
+                inputs = settings["preprocessor"](image_obj, return_tensors="pt").to(device)
+
+                with torch.no_grad():
+                    output = settings["model"](**inputs).logits
+                    scores = softmax(output[0].cpu().numpy())
+                    metadata.update({classifier: {
+                        settings["model"].config.id2label[i]: score.item() for i, score in enumerate(scores) if score > args.label_threshold}
+                    })
+
+            previous = True
+            done += 1
+
+            outfile.write(json.dumps({image.name: metadata}) + "\n")
